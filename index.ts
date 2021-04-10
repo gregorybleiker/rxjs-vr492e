af4d868622bca2a6514ef2c6f69e04ad56dc6582
gregorybleiker <gregory@chocoholics.ch>
2021-04-10 15:07:01 +0200

    1 file modified

--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
-import { Observable } from 'rxjs'; 
+import { of } from 'rxjs'; 
 import { take } from 'rxjs/operators';
 
-let origin = Observable.create((x) => x.next(100));
+let origin = of(100);
 let processor = origin.pipe(take(1));
 processor.subscribe((x) => console.log(x));
